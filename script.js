2081d920ebdbe3edda35976eac190776d1aca6d1
vogana <cctv92@126.com>
2023-08-03 01:38:54 +0800

    vog

--- script.js
+++ script.js
@@ -2,10 +2,17 @@
 const backToTopBtn = document.getElementById("backToTopBtn");
 
 // 绑定按钮点击事件
-backToTopBtn.addEventListener("click", function() {
-    // 使用smooth滚动到顶部
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth"
-    });
-});
+backToTopBtn.addEventListener("click", scrollToTop);
+
+// 滚动到顶部的函数
+function scrollToTop() {
+    const scrollDuration = 300; // 滚动持续时间，以毫秒为单位
+    const scrollStep = -window.scrollY / (scrollDuration / 15);
+    const scrollInterval = setInterval(function() {
+        if (window.scrollY !== 0) {
+            window.scrollBy(0, scrollStep);
+        } else {
+            clearInterval(scrollInterval);
+        }
+    }, 15);
+}
