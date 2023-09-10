04f91fe05d30e64c9dbe5abb6ced7985bbf27f8a
vogana <cctv92@126.com>
2023-09-10 15:24:02 +0800

    vog

---
+++ js/footer.js
@@ -0,0 +1,34 @@
+// footer.js
+export const footerContent = `
+  <footer>
+    <div class="footer">
+  <div class="desktop-content">2023 ANNAZI版权所有 cctv92@126.com </div>
+  <div class="mobile-content">
+    <footer class="footer">
+      <nav class="footer-menu">
+        <ul class="menu-list">
+          <li><a href="https://sp.annazi.link:10445/spwy" target="_blank">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" style="fill: #eb6d6e;" class="bi bi-camera-reels-fill" viewBox="0 0 16 16">
+              <path d="M6 3a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+              <path d="M9 6a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
+              <path d="M9 6h.5a2 2 0 0 1 1.983 1.738l3.11-1.382A1 1 0 0 1 16 7.269v7.462a1 1 0 0 1-1.406.913l-3.111-1.382A2 2 0 0 1 9.5 16H2a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h7z"/>
+            </svg>
+            自媒体</a></li>
+          <li><a href="fuwu.html" target="_blank">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" style="fill: #eb6d6e;" class="bi bi-emoji-smile-fill" viewBox="0 0 16 16">
+              <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zM7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5zM4.285 9.567a.5.5 0 0 1 .683.183A3.498 3.498 0 0 0 8 11.5a3.498 3.498 0 0 0 3.032-1.75.5.5 0 1 1 .866.5A4.498 4.498 0 0 1 8 12.5a4.498 4.498 0 0 1-3.898-2.25.5.5 0 0 1 .183-.683zM10 8c-.552 0-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5S10.552 8 10 8z"/>
+            </svg>
+            服务</a></li>
+          <li><a href="lianxi.html" target="_blank">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" style="fill: #eb6d6e;" class="bi bi-person-fill" viewBox="0 0 16 16">
+              <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+            </svg>
+            联系我</a></li>
+        </ul>
+      </nav>
+    </footer>
+  </div>
+</div>
+
+  </footer>
+`;
---
+++ js/main.js
@@ -0,0 +1,18 @@
+// 在 main.js 文件中，您可以导入共享的页头和页脚内容
+import { headerContent } from './header.js';
+import { footerContent } from './footer.js';
+
+// 在页面加载完成后，将页头和页脚内容插入到相应位置
+document.addEventListener('DOMContentLoaded', function () {
+  // 插入页头内容
+  const headerElement = document.querySelector('header');
+  if (headerElement) {
+    headerElement.innerHTML = headerContent;
+  }
+
+  // 插入页脚内容
+  const footerElement = document.querySelector('footer');
+  if (footerElement) {
+    footerElement.innerHTML = footerContent;
+  }
+});
